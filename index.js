8b37edf8f018fef88a0cc17d2d3d2f6b8b92777e
parfors <suprun.atom@gmail.com>
2023-06-12 14:43:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// const promise = () => {
+//   return new Promise((res, reg) => {
+//     if (true) {
+//       console.log("first");
+//       return res("hello");
+//     }
+//   });
+// };
+
+// promise().then((data) => console.log(data));
+
+// const promise2 = new Promise((res, reg) => {
+//   if (true) {
+//     console.log("first2");
+//     return res("hello2");
+//   }
+// });
+
+// promise2.then((data) => console.log(data));
+
+// console.log(String(3) + String(3));
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+//   ((i) =>
+//     setTimeout(() => {
+//       console.log("async", i);
+//     }, 0))(i);
+// }
